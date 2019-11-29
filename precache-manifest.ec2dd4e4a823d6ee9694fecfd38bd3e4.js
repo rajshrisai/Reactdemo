0feea0b49754ba230628311756cf31bbb4ae7f2c
rajshrisai <rajshrisai@gmail.com>
2019-11-29 15:40:42 +0100

    Updates

---
+++ precache-manifest.ec2dd4e4a823d6ee9694fecfd38bd3e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dd846cbde12c16567d730ff3a7cd31c",
+    "url": "/Reactdemo/index.html"
+  },
+  {
+    "revision": "873a06f49f5b50b5bbb9",
+    "url": "/Reactdemo/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "39da9e06bdd8e5c0b91e",
+    "url": "/Reactdemo/static/js/2.8f97a813.chunk.js"
+  },
+  {
+    "revision": "873a06f49f5b50b5bbb9",
+    "url": "/Reactdemo/static/js/main.f9bd8cdb.chunk.js"
+  },
+  {
+    "revision": "51f60f0c96ec8e5ead6e",
+    "url": "/Reactdemo/static/js/runtime-main.dabb0998.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Reactdemo/static/media/logo.25bf045c.svg"
+  }
+]);
